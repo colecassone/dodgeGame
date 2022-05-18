aefb55c052933234276bb6da4da795a90ba17466
colecassone <colecass651@ed.amdsb.ca>
2022-05-18 14:30:13 -0400

    Add project files.

---
+++ dodgeGame/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace dodgeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subTitleLabel = new System.Windows.Forms.Label();
+            this.leftPersonScore = new System.Windows.Forms.Label();
+            this.rightPersonScore = new System.Windows.Forms.Label();
+            this.easyButton = new System.Windows.Forms.Button();
+            this.midButton = new System.Windows.Forms.Button();
+            this.hardButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.Red;
+            this.titleLabel.Location = new System.Drawing.Point(159, 92);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(0, 19);
+            this.titleLabel.TabIndex = 3;
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // subTitleLabel
+            // 
+            this.subTitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subTitleLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTitleLabel.ForeColor = System.Drawing.Color.Green;
+            this.subTitleLabel.Location = new System.Drawing.Point(144, 161);
+            this.subTitleLabel.Name = "subTitleLabel";
+            this.subTitleLabel.Size = new System.Drawing.Size(186, 126);
+            this.subTitleLabel.TabIndex = 4;
+            this.subTitleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // leftPersonScore
+            // 
+            this.leftPersonScore.AutoSize = true;
+            this.leftPersonScore.BackColor = System.Drawing.Color.Transparent;
+            this.leftPersonScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leftPersonScore.ForeColor = System.Drawing.Color.White;
+            this.leftPersonScore.Location = new System.Drawing.Point(12, 366);
+            this.leftPersonScore.Name = "leftPersonScore";
+            this.leftPersonScore.Size = new System.Drawing.Size(24, 25);
+            this.leftPersonScore.TabIndex = 5;
+            this.leftPersonScore.Text = "0";
+            this.leftPersonScore.Visible = false;
+            // 
+            // rightPersonScore
+            // 
+            this.rightPersonScore.AutoSize = true;
+            this.rightPersonScore.BackColor = System.Drawing.Color.Transparent;
+            this.rightPersonScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rightPersonScore.ForeColor = System.Drawing.Color.White;
+            this.rightPersonScore.Location = new System.Drawing.Point(464, 366);
+            this.rightPersonScore.Name = "rightPersonScore";
+            this.rightPersonScore.Size = new System.Drawing.Size(24, 25);
+            this.rightPersonScore.TabIndex = 6;
+            this.rightPersonScore.Text = "0";
+            this.rightPersonScore.Visible = false;
+            // 
+            // easyButton
+            // 
+            this.easyButton.BackColor = System.Drawing.Color.White;
+            this.easyButton.ForeColor = System.Drawing.Color.Black;
+            this.easyButton.Location = new System.Drawing.Point(63, 264);
+            this.easyButton.Name = "easyButton";
+            this.easyButton.Size = new System.Drawing.Size(75, 23);
+            this.easyButton.TabIndex = 7;
+            this.easyButton.Text = "EASY";
+            this.easyButton.UseVisualStyleBackColor = false;
+            this.easyButton.Click += new System.EventHandler(this.easyButton_Click);
+            // 
+            // midButton
+            // 
+            this.midButton.BackColor = System.Drawing.Color.Gray;
+            this.midButton.ForeColor = System.Drawing.Color.Black;
+            this.midButton.Location = new System.Drawing.Point(203, 264);
+            this.midButton.Name = "midButton";
+            this.midButton.Size = new System.Drawing.Size(75, 23);
+            this.midButton.TabIndex = 8;
+            this.midButton.Text = "MEDIUM";
+            this.midButton.UseVisualStyleBackColor = false;
+            this.midButton.Click += new System.EventHandler(this.midButton_Click);
+            // 
+            // hardButton
+            // 
+            this.hardButton.BackColor = System.Drawing.Color.Gray;
+            this.hardButton.ForeColor = System.Drawing.Color.Black;
+            this.hardButton.Location = new System.Drawing.Point(346, 264);
+            this.hardButton.Name = "hardButton";
+            this.hardButton.Size = new System.Drawing.Size(75, 23);
+            this.hardButton.TabIndex = 9;
+            this.hardButton.Text = "HARD";
+            this.hardButton.UseVisualStyleBackColor = false;
+            this.hardButton.Click += new System.EventHandler(this.hardButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Gray;
+            this.startButton.ForeColor = System.Drawing.Color.Black;
+            this.startButton.Location = new System.Drawing.Point(203, 310);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 10;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // quitButton
+            // 
+            this.quitButton.BackColor = System.Drawing.Color.Gray;
+            this.quitButton.ForeColor = System.Drawing.Color.Black;
+            this.quitButton.Location = new System.Drawing.Point(422, 340);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(75, 23);
+            this.quitButton.TabIndex = 11;
+            this.quitButton.Text = "QUIT";
+            this.quitButton.UseVisualStyleBackColor = false;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(500, 400);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.hardButton);
+            this.Controls.Add(this.midButton);
+            this.Controls.Add(this.easyButton);
+            this.Controls.Add(this.rightPersonScore);
+            this.Controls.Add(this.leftPersonScore);
+            this.Controls.Add(this.subTitleLabel);
+            this.Controls.Add(this.titleLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subTitleLabel;
+        private System.Windows.Forms.Label leftPersonScore;
+        private System.Windows.Forms.Label rightPersonScore;
+        private System.Windows.Forms.Button easyButton;
+        private System.Windows.Forms.Button midButton;
+        private System.Windows.Forms.Button hardButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button quitButton;
+    }
+}
+
---
+++ dodgeGame/Form1.cs
@@ -0,0 +1,393 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Windows.Forms;
+
+namespace dodgeGame
+{
+
+
+    public partial class Form1 : Form
+    {
+        Rectangle rightHero = new Rectangle(205, 350, 10, 10);
+        Rectangle leftHero = new Rectangle(295, 350, 10, 10);
+        Rectangle midLine = new Rectangle(250, 300, 5, 100);
+        Pen drawPen = new Pen(Color.White, 5);
+
+        bool leftDown = false;
+        bool rightDown = false;
+        bool upDown = false;
+        bool downDown = false;
+        bool wDown = false;
+        bool sDown = false;
+        List<Rectangle> leftBalls = new List<Rectangle>();
+        List<Rectangle> rightBalls = new List<Rectangle>();
+
+        int heroSpeed = 5;
+        int leftSpeed = 8;
+        int rightSpeed = 8;
+
+
+        int WballSize = 3;
+        int LballSize = 3;
+        int leftCounter = 0;
+        int rightCounter = 0;
+        // 1 == waiting 
+        // 2 == lost
+        // 3 === P1winner 
+        // 4 == P2winner
+        int gameState = 1;
+
+
+        int leftScore = 0;
+        int rightScore = 0;
+        int numBall = 60;
+
+        Random randGen = new Random();
+
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void GameInitialize()
+        {
+            // starting the game
+            upDown = false;
+            downDown = false;
+            wDown = false;
+            sDown = false;
+
+            leftPersonScore.Visible = true;
+            rightPersonScore.Visible = true;
+
+            titleLabel.Text = "";
+            subTitleLabel.Text = "";
+
+            gameTimer.Enabled = true;
+            gameState = 2;
+
+            leftHero.X = 205;
+            leftHero.Y = 350;
+
+            rightHero.X = 295;
+            rightHero.Y = 350;
+
+            rightPersonScore.Text = "0";
+            leftPersonScore.Text = "0";
+
+            leftScore = 0;
+            rightScore = 0;
+
+            easyButton.Visible = false;
+            midButton.Visible = false;
+            hardButton.Visible = false;
+            startButton.Visible = false;
+
+            leftBalls.Clear();
+            rightBalls.Clear();
+
+
+        }
+
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        { // to check what are being pushed 
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.Right:
+                    rightDown = false;
+                    break;
+                case Keys.Down:
+                    downDown = false;
+                    break;
+                case Keys.Up:
+                    upDown = false;
+                    break;
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            { // to check what are being pushed 
+                case Keys.Down:
+                    downDown = true;
+                    break;
+                case Keys.Up:
+                    upDown = true;
+                    break;
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // waiting for game to sart and to see the level 
+            if (gameState == 1)
+            {
+                titleLabel.Text = "THIS IS THE GAME";
+                subTitleLabel.Text = "Press Space Bar to Start or Q to Exit";
+            }
+            //The game is running in this state 
+            else if (gameState == 2)
+            {
+                titleLabel.Text = "";
+                subTitleLabel.Text = "";
+                e.Graphics.FillRectangle(whiteBrush, leftHero);
+                e.Graphics.FillRectangle(whiteBrush, rightHero);
+                e.Graphics.FillRectangle(whiteBrush, midLine);
+                for (int i = 0; i < leftBalls.Count(); i++)
+                {
+                    e.Graphics.FillRectangle(whiteBrush, leftBalls[i]);
+                }
+                for (int i = 0; i < rightBalls.Count(); i++)
+                {
+                    e.Graphics.FillRectangle(whiteBrush, rightBalls[i]);
+                }
+            }
+            // right player won the game and gives the option to restart or quit and choose the level 
+            else if (gameState == 3)
+            {
+                gameTimer.Stop();
+                leftPersonScore.Visible = false;
+                rightPersonScore.Visible = false;
+                easyButton.Visible = true;
+                midButton.Visible = true;
+                hardButton.Visible = true;
+                startButton.Visible = true;
+                quitButton.Visible = true;
+                easyButton.Enabled = true;
+                midButton.Enabled = true;
+                hardButton.Enabled = true;
+                startButton.Enabled = true;
+                quitButton.Enabled = true;
+
+
+                titleLabel.Text = "Left Player Won";
+                subTitleLabel.Text = "Press Space Bar to Start Over or Q to Exit";
+
+            }
+            // Left player won the game and gives the option to restart or quit and choose the level 
+            else
+            {
+                leftPersonScore.Visible = false;
+                rightPersonScore.Visible = false;
+                gameTimer.Stop();
+                easyButton.Visible = true;
+                midButton.Visible = true;
+                hardButton.Visible = true;
+                startButton.Visible = true;
+                quitButton.Visible = true;
+                easyButton.Enabled = true;
+                midButton.Enabled = true;
+                hardButton.Enabled = true;
+                startButton.Enabled = true;
+                quitButton.Enabled = true;
+
+                titleLabel.Text = "Right Player Won";
+                subTitleLabel.Text = "Press Space Bar to Start Over or Q to Exit";
+            }
+
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //lefthHero move up or down when the up or down keys are pressed.
+            if (upDown == true)
+            {
+                rightHero.Y -= heroSpeed;
+            }
+
+            if (downDown == true && rightHero.Y < 390)
+            {
+                rightHero.Y += heroSpeed;
+            }
+
+            // rightHero move when the W or S keys are pressed 
+            if (wDown == true && leftHero.Y > 0)
+            {
+                leftHero.Y -= heroSpeed;
+            }
+
+            if (sDown == true && leftHero.Y < 390)
+            {
+                leftHero.Y += heroSpeed;
+            }
+            // Adding balls to the list and uses a counter to limit the amount of balls per time 
+            leftCounter++;
+            rightCounter++;
+
+            if (leftCounter == numBall)
+            {
+                int leftRandom = randGen.Next(1, 301);
+
+                leftBalls.Add(new Rectangle(0, leftRandom, LballSize, WballSize));
+                leftCounter = 0;
+            }
+
+            if (rightCounter == numBall)
+            {
+                int rightRandom = randGen.Next(1, 301);
+
+                rightBalls.Add(new Rectangle(500, rightRandom, LballSize, WballSize));
+                rightCounter = 0;
+            }
+
+            // moving the left side balls right to left
+            for (int i = 0; i < leftBalls.Count; i++)
+            {
+                int x = leftBalls[i].X + leftSpeed;
+                leftBalls[i] = (new Rectangle(x, leftBalls[i].Y, LballSize, WballSize));
+            }
+            // moveing the right side balls left to right 
+            for (int i = 0; i < rightBalls.Count; i++)
+            {
+                int x = rightBalls[i].X - rightSpeed;
+                rightBalls[i] = (new Rectangle(x, rightBalls[i].Y, LballSize, WballSize));
+            }
+            // this is check the heros and balls to see of there is intersections and if there is play a sound a reset hero 
+            SoundPlayer wrong = new SoundPlayer(Properties.Resources.wrong);
+
+            for (int i = 0; i < rightBalls.Count; i++)
+            {
+                if (leftHero.IntersectsWith(rightBalls[i]))
+                {
+                    wrong.Play();
+                    leftHero.Y = 350;
+                }
+
+                if (rightHero.IntersectsWith(rightBalls[i]))
+                {
+                    wrong.Play();
+                    rightHero.Y = 350;
+                }
+            }
+            // this is check the heros and balls to see of there is intersections and if there is play a sound a reset hero 
+            for (int i = 0; i < leftBalls.Count; i++)
+            {
+                if (leftHero.IntersectsWith(leftBalls[i]))
+                {
+                    wrong.Play();
+
+                    leftHero.Y = 350;
+                }
+
+                if (rightHero.IntersectsWith(leftBalls[i]))
+                {
+                    wrong.Play();
+
+                    rightHero.Y = 350;
+                }
+            }
+
+            SoundPlayer player = new SoundPlayer(Properties.Resources.someoneWon);
+            SoundPlayer playerGG = new SoundPlayer(Properties.Resources.bell);
+
+            // See if left hero has hit the back and if he has add to the score and if the score is greater then 3 go to a different game state 
+            if (leftHero.Y == 0)
+            {
+                playerGG.Play();
+                leftScore++;
+
+                if (leftScore == 3)
+                {
+                    player.Play();
+                    gameState = 3;
+                }
+                else
+                {
+                    leftPersonScore.Text = $"{leftScore}";
+                    leftHero.Y = 350;
+                }
+
+            }
+            // See if right hero has hit the back and if he has add to the score and if the score is greater then 3 go to a different game state 
+            if (rightHero.Y == 0)
+            {
+                rightScore++;
+                playerGG.Play();
+
+                if (rightScore == 3)
+                {
+                    gameState = 4;
+                    player.Play();
+                }
+                else
+                {
+                    rightPersonScore.Text = $"{rightScore}";
+                    rightHero.Y = 350;
+                }
+
+            }
+
+            Refresh();
+
+        }
+
+        private void easyButton_Click(object sender, EventArgs e)
+        {
+            midButton.BackColor = Color.Gray;
+            hardButton.BackColor = Color.Gray;
+            easyButton.BackColor = Color.White;
+            numBall = 60;
+        }
+
+        private void midButton_Click(object sender, EventArgs e)
+        {
+
+            midButton.BackColor = Color.White;
+            hardButton.BackColor = Color.Gray;
+            easyButton.BackColor = Color.Gray;
+            numBall = 13;
+        }
+
+        private void hardButton_Click(object sender, EventArgs e)
+        {
+
+            midButton.BackColor = Color.Gray;
+            hardButton.BackColor = Color.White;
+            easyButton.BackColor = Color.Gray;
+            numBall = 4;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+
+            midButton.Enabled = false;
+            hardButton.Enabled = false;
+            easyButton.Enabled = false;
+            midButton.Visible = false;
+            hardButton.Visible = false;
+            easyButton.Visible = false;
+            startButton.Enabled = false;
+            startButton.Visible = false;
+            quitButton.Enabled = false;
+            quitButton.Visible = false;
+            GameInitialize();
+            this.Focus();
+        }
+
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
